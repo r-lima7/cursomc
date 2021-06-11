ad0514da35f924963f6867371b1b76101604b889
Rodolfo Lima <rlimadesbrava@gmail.com>
2021-06-11 12:12:46 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/rlima/cursomc/services/CategoriaService.java
+++ src/main/java/com/rlima/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rlima.cursomc.domain.Categoria;
+import com.rlima.cursomc.dto.CategoriaDTO;
 import com.rlima.cursomc.repositories.CategoriaRepository;
 import com.rlima.cursomc.services.exceptions.DataIntegrityException;
 import com.rlima.cursomc.services.exceptions.ObjectNotFoundException;
@@ -65,6 +66,12 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 		
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	
 	
 	
 	
