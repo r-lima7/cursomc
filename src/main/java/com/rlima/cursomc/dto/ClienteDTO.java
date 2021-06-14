44a13a829ca7fb97f685829fc2287e8c7b969b45
Rodolfo Lima <rlimadesbrava@gmail.com>
2021-06-14 11:23:38 -0300

    Validacao de CPF ou CNPJ ao inserir novo Cliente

--- src/main/java/com/rlima/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rlima/cursomc/dto/ClienteDTO.java
@@ -18,7 +18,7 @@ public class ClienteDTO implements Serializable {
 	@Length(min = 5,max = 120,  message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
-	@NotEmpty
+	@NotEmpty(message = "Preenchimento obrigatório")
 	@Email(message = "Email invalido!")
 	private String email;
 
